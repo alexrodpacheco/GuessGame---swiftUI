b598c9529f00a878cff5b86336bd909c4bba3c2f
Alex Pacheco <alexrodpacheco@gmail.com>
2025-07-29 18:16:41 -0300

    first commit

---
+++ GuessGame-SwiftUI/GuessGame-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessGame-SwiftUI
+//
+//  Created by Alex Rodrigues Pacheco on 29/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessGame-SwiftUI/GuessGame-SwiftUI/GuessGame_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGame_SwiftUIApp.swift
+//  GuessGame-SwiftUI
+//
+//  Created by Alex Rodrigues Pacheco on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessGame_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
